de20e06bbe4199e13228e92e5055dfeee74a4104
Makan DIANKA <makan.dianka@hotmail.com>
2023-01-27 21:23:02 +0100

    create usermail model

--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import (
+    UserEmail,
+    UserEmailAdmin
+)
 
-# Register your models here.
+admin.site.register(UserEmail, UserEmailAdmin)
--- research/models.py
+++ research/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class UserEmail(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+    valid = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True, null=True)
+
+class UserEmailAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'code', 'valid']
