5014eae8d32c8caa2e521e716df0f2dad1a7ad04
Makan DIANKA <makan.dianka@hotmail.com>
2023-01-28 20:37:40 +0100

    give a parametre to alert url

--- research/urls.py
+++ research/urls.py
@@ -5,7 +5,7 @@ app_name="research"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('alert', views.alert, name="alert"),
+    path('alert/<int:id>', views.alert, name="alert"),
     path('p/<str:base64>', views.confirm_email, name="confirm_email"),
     path('preference', views.preference, name="preference"),
 ]
