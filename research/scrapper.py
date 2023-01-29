bad25cc9cbb9a33a4b11202c7bba96f83bb2d2d3
Makan DIANKA <makan.dianka@hotmail.com>
2023-01-30 00:30:10 +0100

    optimize code, create a class

--- research/scrapper.py
+++ research/scrapper.py
@@ -1,64 +1,64 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.hellowork.com/emploi/recherche.html"
-user_agent = {'user-agent' : 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36'}
+class HelloWork:
+    def __init__(self):
+        self.url = "https://www.hellowork.com/emploi/recherche.html"
+        self.user_agent = {'user-agent' : 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36'}
+        self.session = requests.Session()
 
-s = requests.Session()
+    def find_links(self, lang, location):
+        host = "https://www.hellowork.com"
+        params = {'k' : lang, 'l':location}
+        links = []
 
-def find_links(lang, location):
-    host = "https://www.hellowork.com"
-    params = {'k' : lang, 'l':location}
-    links = []
+        res = self.session.get(self.url, headers=self.user_agent, params=params)
+        if res.ok:
+            soup = BeautifulSoup(res.text, "html.parser")
+            content = soup.find('ul', class_="crushed")
+            element_li = content.find_all('li', class_="!tw-mb-6")
+            for element in element_li:
+                div = element.find('div', class_='crushed')
+                div2_1 = div.find('div', class_="offer--maininfo")
+                info = div2_1.find("h3", class_="!tw-mb-0")
+                # h3 = info.text
+                a = info.find('a')['href']
+                links.append(host+a)
+        return links
 
-    res = s.get(url, headers=user_agent, params=params)
-    if res.ok:
-        soup = BeautifulSoup(res.text, "html.parser")
-        content = soup.find('ul', class_="crushed")
-        element_li = content.find_all('li', class_="!tw-mb-6")
-        for element in element_li:
-            div = element.find('div', class_='crushed')
-            div2_1 = div.find('div', class_="offer--maininfo")
-            info = div2_1.find("h3", class_="!tw-mb-0")
-            # h3 = info.text
-            a = info.find('a')['href']
-            links.append(host+a)
-    return links
+    def scrap(self, lang:str, location:str)->list:
+        """return une liste de dictionnaire"""
+        links = self.find_links(lang, location)
+        data = []
+        if links:
+            for link in links:
+                data_dict = {}
+                res = self.session.get(link, headers=self.user_agent)
+                if res.ok:
+                    soup = BeautifulSoup(res.text, "html.parser")
+                    section = soup.find('section', class_="campagne")
+                    h1 = section.find('h1', class_="tw-flex-col")
+                    span = h1.find('span', class_="!tw-text-4xlOld")
 
+                    data_dict["title"] = span.text
 
-def scrap(lang:str, location:str)->list:
-    """return une liste de dictionnaire"""
-    links = find_links(lang, location)
-    data = []
-    if links:
-        for link in links:
-            data_dict = {}
-            res = s.get(link, headers=user_agent)
-            if res.ok:
-                soup = BeautifulSoup(res.text, "html.parser")
-                section = soup.find('section', class_="campagne")
-                h1 = section.find('h1', class_="tw-flex-col")
-                span = h1.find('span', class_="!tw-text-4xlOld")
+                    li = section.find_all('li')
+                    city = li[0].find('span')
+                    contr = li[1].find('span')
 
-                data_dict["title"] = span.text
+                    data_dict['city'] = city.text.strip()
+                    data_dict['contrat'] = contr.text.strip()
 
-                li = section.find_all('li')
-                city = li[0].find('span')
-                contr = li[1].find('span')
+                    main = soup.find('main')
+                    section1 = main.find('section')
+                    h2 = section1.find('h2')
+                    p = section1.find('p')
+                    date = section1.find('span', class_="retrait").find('span')
 
-                data_dict['city'] = city.text.strip()
-                data_dict['contrat'] = contr.text.strip()
+                    data_dict["minititre"] = h2.text.strip()
+                    data_dict['detail'] = p.text.strip()
+                    data_dict['date'] = date.text
 
-                main = soup.find('main')
-                section1 = main.find('section')
-                h2 = section1.find('h2')
-                p = section1.find('p')
-                date = section1.find('span', class_="retrait").find('span')
+                    data.append(data_dict)
 
-                data_dict["minititre"] = h2.text.strip()
-                data_dict['detail'] = p.text.strip()
-                data_dict['date'] = date.text
-
-                data.append(data_dict)
-
-    return data
+        return data
