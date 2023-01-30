06247da3933269fca1ff7d5ebc241b9257caaef3
Makan DIANKA <makan.dianka@hotmail.com>
2023-01-30 15:29:40 +0100

    add config email

--- emploi/settings.py
+++ emploi/settings.py
@@ -154,13 +154,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# # cert secure 
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 LOGGING = {
@@ -198,7 +197,7 @@ LOGGING = {
             'propagate': True,
             'level' : 'DEBUG'
         },
-    'django': {
+        'django': {
             'handlers':['file'],
             'propagate': True,
             'level':'DEBUG',
