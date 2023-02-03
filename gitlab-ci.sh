055ef824911e73ba534011602f4eaca4fdbd9815
Makan DIANKA <makan.dianka@hotmail.com>
2023-02-03 03:04:11 +0100

    deploy

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -5,5 +5,5 @@ set -e
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/gitlab_key
 
-git pull git@gitlab.com:dmakan/job.git master 
+git pull git@gitlab.com:dmakan/job.git
 docker-compose --env-file .env up -d
