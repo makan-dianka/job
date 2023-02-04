d3108594cb7f956825e0c8603e299e99e32ff0ec
Makan DIANKA <makan.dianka@hotmail.com>
2023-02-04 23:54:43 +0100

    update

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -6,6 +6,6 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/gitlab_key
 
 git pull git@gitlab.com:dmakan/job.git
-docker stop "$(docker ps)" && docker rm "$(docker ps -aq)"
+docker stop job && docker rm job
 docker image rm job_app:latest
 docker-compose --env-file .env up -d
